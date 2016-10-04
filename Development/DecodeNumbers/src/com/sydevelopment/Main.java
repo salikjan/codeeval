35aad7e426b743727a6fbe0e730eeb2e6475f17e
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-04 18:01:55 +0500

    Partially solved

--- Development/DecodeNumbers/src/com/sydevelopment/Main.java
+++ Development/DecodeNumbers/src/com/sydevelopment/Main.java
@@ -5,14 +5,24 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         // write your code here
         Scanner sc = new Scanner(new File(args[0]));
         while (sc.hasNextLine()) {
             String line = sc.nextLine().trim();
-            System.out.println(line);
+            int counter = 1;
+            if (line.charAt(0) == '-') {
+                continue;
+            }
+
+            for (int i = 0; i < line.length() - 1; i++) {
+                String s = line.substring(i, i + 1) +
+                        line.substring(i + 1, i + 2);
+                if (Integer.parseInt(s) <= 26) {
+                    counter++;
+                }
+            }
+            System.out.println(counter);
         }
-        System.out.println("");
     }
 }
