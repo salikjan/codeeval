97dd89ca2c3d88dbfad53b5e0e63dcc8de3af322
Salavat Yalalov <Salavat.Yalalov@CIS.RU>
2016-10-04 09:35:26 +0500

    Easy Level

--- Easy/AgeDistribution/Main.java
+++ Easy/AgeDistribution/Main.java
@@ -1,11 +1,10 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    // write your code here
+	// write your code here
         Scanner sc = new Scanner(new File(args[0]));
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
