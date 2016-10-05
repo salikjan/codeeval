5582e06cb12013fc6173d3dd1acd75b203d26158
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-05 15:36:27 +0500

    Thinking on new algorithm

--- Development/DecodeNumbers/src/com/sydevelopment/Main.java
+++ Development/DecodeNumbers/src/com/sydevelopment/Main.java
@@ -10,19 +10,6 @@ public class Main {
         Scanner sc = new Scanner(new File(args[0]));
         while (sc.hasNextLine()) {
             String line = sc.nextLine().trim();
-            int counter = 1;
-            if (line.charAt(0) == '-') {
-                continue;
-            }
-
-            for (int i = 0; i < line.length() - 1; i++) {
-                String s = line.substring(i, i + 1) +
-                        line.substring(i + 1, i + 2);
-                if (Integer.parseInt(s) <= 26) {
-                    counter++;
-                }
-            }
-            System.out.println(counter);
         }
     }
 }
