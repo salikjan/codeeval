618060c302e04ea12dc3811d6ba5031f6f5e0c81
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-06 15:22:14 +0500

    Another version (not working)

--- Development/DecodeNumbers/src/com/sydevelopment/Main.java
+++ Development/DecodeNumbers/src/com/sydevelopment/Main.java
@@ -10,6 +10,41 @@ public class Main {
         Scanner sc = new Scanner(new File(args[0]));
         while (sc.hasNextLine()) {
             String line = sc.nextLine().trim();
+            if (!"".equals(line)) {
+                System.out.println(findWays(line));
+            }
         }
     }
+
+    public static int findWays(String line) {
+        int counter = 0;
+
+        String s1, s2, s3;
+
+        if (line.length() > 1) {
+            s1 = line.substring(0, 1);
+            s2 = line.substring(1, line.length());
+            s3 = s1 + s2.substring(0, 1);
+        } else {
+            return 1;
+        }
+
+        if (s2.length() > 2) {
+            counter++;
+            counter += findWays(s2);
+        }
+
+        if (s2.length() == 2) {
+            counter += findWays(s2);
+        } else if (s2.length() < 2) {
+            counter++;
+        }
+
+        if (Integer.parseInt(s3) <= 26) {
+            counter++;
+        }
+
+        return counter;
+    }
+
 }
