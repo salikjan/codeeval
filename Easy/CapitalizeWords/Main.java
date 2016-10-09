4320b8817cccc6f7b5151b7e834e8d21f26e7d22
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-09 14:28:29 +0500

    Easy level

---
+++ Easy/CapitalizeWords/Main.java
@@ -0,0 +1,31 @@
+package com.sydev;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    // write your code here
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(capitalizeWords(line.trim()));
+        }
+    }
+
+    public static String capitalizeWords(String line) {
+        char arr[] = new char[line.length()];
+
+        arr = line.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            char ch = arr[i];
+            if (i == 0 || Character.isWhitespace(arr[i - 1])) {
+                arr[i] = Character.toUpperCase(ch);
+            }
+        }
+
+        return new String(arr);
+    }
+}
---
+++ Easy/DataRecovery/Main.java
@@ -0,0 +1,48 @@
+package com.sydev;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        Scanner sc = new Scanner(new File(args[0]));
+        while (sc.hasNextLine()) {
+            System.out.println(recoverData(sc.nextLine()));
+        }
+    }
+
+    public static String recoverData(String line) {
+        String[] buffer = line.split(";");
+        String[] words = buffer[0].trim().split(" ");
+        String[] indexes = buffer[1].trim().split(" ");
+
+        //
+        Map<Integer, String> wordsInOrder = new TreeMap<Integer, String>();
+        for (int i = 0; i < indexes.length; i++) {
+            wordsInOrder.put(Integer.parseInt(indexes[i]), words[i]);
+        }
+        //
+        for (int i = 1; i <= words.length; i++) {
+            if (!wordsInOrder.containsKey(i)) {
+                wordsInOrder.put(i, words[words.length - 1]);
+            }
+        }
+        //
+        int index = 0;
+        StringBuilder result = new StringBuilder("");
+        for (Integer key : wordsInOrder.keySet()) {
+            result.append(wordsInOrder.get(key));
+            if (index < wordsInOrder.keySet().size() - 1) {
+                result.append(" ");
+                index++;
+            }
+        }
+
+        return result.toString();
+    }
+}
