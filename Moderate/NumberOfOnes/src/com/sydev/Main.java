fa21b62985d371b3e9ad0c6f548c5b966a79df8a
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-09 18:21:31 +0500

    CodeEval

--- Moderate/NumberOfOnes/src/com/sydev/Main.java
+++ Moderate/NumberOfOnes/src/com/sydev/Main.java
@@ -1,8 +1,16 @@
 package com.sydev;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        Scanner sc = new Scanner(new File(args[0]));
+        while (sc.hasNextLine()) {
+            int n = Integer.parseInt(sc.nextLine());
+            System.out.println(Integer.bitCount(n));
+        }
     }
 }
