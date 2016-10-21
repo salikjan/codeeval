b61c641ffd05e0c5191d7cc166a05010af9a591e
Salavat Yalalov <s.yalalov@gmail.com>
2016-10-21 08:41:55 +0500

    Find A Square moderate challenge

--- development/FindSquare/src/Main.java
+++ development/FindSquare/src/Main.java
@@ -15,17 +15,17 @@ public class Main {
         while (sc.hasNextLine()) {
             Rectangle r = new Rectangle(sc.nextLine());
             if (r.isSquare()) {
-                System.out.println("false");
-            } else {
                 System.out.println("true");
+            } else {
+                System.out.println("false");
             }
         }
     }
 }
 
 class Point {
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -43,30 +43,33 @@ class Rectangle {
 
         if (m.find()) {
             String[] s = m.group(1).trim().split(",");
-            Point point = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            this.p1 = point;
+            this.p1 = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
 
             s = m.group(2).trim().split(",");
-            point = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            this.p2 = point;
+            this.p2 = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
 
             s = m.group(3).trim().split(",");
-            point = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            this.p3 = point;
+            this.p3 = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
 
             s = m.group(4).trim().split(",");
-            point = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            this.p4 = point;
+            this.p4 = new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
         } else {
             System.out.println("NO MATCH!");
         }
     }
 
     public boolean isSquare() {
-        boolean isX = false;
-        boolean isY = false;
+        long dlp1p2 = (long)(Math.sqrt(Math.pow(this.p1.x - this.p2.x, 2) - Math.pow(this.p1.y - this.p2.y, 2)) * 10000);
+        long dlp2p3 = (long)(Math.sqrt(Math.pow(this.p2.x - this.p3.x, 2) - Math.pow(this.p2.y - this.p3.y, 2)) * 10000);
+        long dlp3p4 = (long)(Math.sqrt(Math.pow(this.p3.x - this.p4.x, 2) - Math.pow(this.p3.y - this.p4.y, 2)) * 10000);
+        long dlp4p1 = (long)(Math.sqrt(Math.pow(this.p4.x - this.p1.x, 2) - Math.pow(this.p4.y - this.p1.y, 2)) * 10000);
+        long dlp1p3 = (long)(Math.sqrt(Math.pow(this.p1.x - this.p3.x, 2) - Math.pow(this.p1.y - this.p3.y, 2)) * 10000);
+        long dlp2p4 = (long)(Math.sqrt(Math.pow(this.p2.x - this.p4.x, 2) - Math.pow(this.p2.y - this.p4.y, 2)) * 10000);
 
-
-        return true;
+        if (dlp1p2 == dlp2p3 && dlp2p3 == dlp3p4 && dlp3p4 == dlp4p1 && dlp1p3 == dlp2p4) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
